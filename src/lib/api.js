7dff70615a6c426b89e913f09bdcb9907ebcc69e
Aayush Neupane <neupane0403@gmail.com>
2022-10-17 17:20:57 +0545

    axios refactor

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,13 +1,4 @@
 import axios from "axios";
-console.log("react base ur", process.env.REACT_APP_BASE_URL);
-//  axios.create({
-//   baseURL: process.env.REACT_APP_BASE_URL,
-
-// });
-// create axios client for json request
 export default axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
